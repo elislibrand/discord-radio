0b3804530588389477e273635e539d4b590b0258
Elis Librand <elis.librand@gmail.com>
2021-04-01 21:18:59 +0200

    Fix problem with embed

--- main.py
+++ main.py
@@ -167,7 +167,7 @@ class Radio(commands.Cog):
                 embed.set_thumbnail(url = 'https://images.vexels.com/media/users/3/132597/isolated/preview/e8c7c6b823f6df05ec5ae37ea03a5c88-vinyl-record-icon-by-vexels.png')
                 embed.set_footer(text = datetime.now().strftime('%Y-%m-%d %H:%M'))
 
-                await ctx.send(embed)
+                await ctx.send(embed = embed)
                 
                 #await ctx.send('>>> Currently playing **{}** by **{}**'.format(song, artist))
         else:
